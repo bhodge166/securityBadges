5bc08ba5e3868f7049ee901758adef25a63a6f13
Brad Hodge <bhodge166@gmail.com>
2022-05-18 15:30:39 -0600

    moved print function to utils class to clean up code

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace CatWorx.BadgeMaker
         {
 
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
         }
 
         static List<Employee> GetEmployees()
@@ -35,16 +35,6 @@ namespace CatWorx.BadgeMaker
             }
             // This is important!
             return employees;
-        }
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-
-            }
         }
     }
 }
