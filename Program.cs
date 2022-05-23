c439cd98714474eff24d9a334d0793a5a8f1319b
Brad Hodge <bhodge166@gmail.com>
2022-05-23 15:31:25 -0600

    added option to get random employee data from api. moved function for organization purposes

--- Program.cs
+++ Program.cs
@@ -8,35 +8,11 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         {
 
-            List<Employee> employees = GetEmployees();
+            List<Employee> employees = PeopleFetcher.GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
         }
 
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string firstName = Console.ReadLine();
-                if (firstName == "")
-                {
-                    break;
-                }
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine();
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine();
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-
-            }
-            // This is important!
-            return employees;
-        }
     }
 }
